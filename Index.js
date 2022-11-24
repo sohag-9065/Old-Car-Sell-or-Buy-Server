23d3b442e899f96aa0fdf8e66f18d8e82883c3ac
Sohag <sohaghossain7th@gmail.com>
2022-11-24 19:11:12 +0600

    Mongodb Connect

--- Index.js
+++ Index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,25 @@ const port = process.env.PORT;
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+// mongoDB connet uri 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.akdywg4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        // create database collection 
+        const Brand = client.db('oldCar').collection('brand');
+
+        
+        console.log("DB connect")
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("Hello from Old Car!")
