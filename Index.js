9a6ca994fe296c863493dd13956d7fb2c4ca4dad
Sohag <sohaghossain7th@gmail.com>
2022-11-24 18:29:39 +0600

    middleware and listening port added

--- Index.js
+++ Index.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello from Old Car!")
+})
+
+app.listen(port, () => {
+    console.log(`Old Car app listening on port ${port}`)
+})
+
 
